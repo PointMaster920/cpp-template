0b79e889b87658f8ec8aa3860cb8106c01c36aa0
讨厌下雨天 <x3519610554@outlook.com>
2025-10-06 21:56:46 +0800

    ✨ feat: add thread_pool

--- src/app/main.cpp
+++ src/app/main.cpp
@@ -1,6 +1,29 @@
 #include <iostream>
+#include <chrono>
+#include "thread_pool.h"
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    ThreadPool::instance()->initialize(10);
+    ThreadPool::instance()->start();
+    ThreadPool::instance()->enqueue([](){
+        while (ThreadPool::instance()->get_state()){
+            std::cout << "Thread1 is running..." << std::endl;
+            std::this_thread::sleep_for(std::chrono::milliseconds(500));
+        }
+    });
+    ThreadPool::instance()->enqueue([](){
+        while (ThreadPool::instance()->get_state()){
+            std::cout << "Thread2 is running..." << std::endl;
+            std::this_thread::sleep_for(std::chrono::milliseconds(500));
+        }
+    });
+    ThreadPool::instance()->enqueue([](){
+        while (ThreadPool::instance()->get_state()){
+            std::cout << "Thread3 is running..." << std::endl;
+            std::this_thread::sleep_for(std::chrono::milliseconds(500));
+        }
+    });
+    ThreadPool::instance()->stop();
+    ThreadPool::instance()->wait();
     return 0;
 }
